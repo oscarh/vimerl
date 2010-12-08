d2962135b3a1d8ddb871f28393ec921713c63644
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2010-12-08 20:24:13 +0100

    Improve syntax check script
    Using basic_validation for faster check times. Add the "include" and
    "../include" directories in order to get .hrl files.

--- compiler/erlang_check_file.erl
+++ compiler/erlang_check_file.erl
@@ -1,8 +1,4 @@
 #!/usr/bin/env escript
--export([main/1]).
 
-main([File_Name]) ->
-    compile:file(File_Name, [warn_obsolete_guard,
-                            warn_unused_import,
-                            warn_shadow_vars, warn_export_vars,
-                            strong_validation, report]).
+main([Filename]) ->
+	compile:file(Filename, [basic_validation, report, {i, "include"}, {i, "../include"}]).
