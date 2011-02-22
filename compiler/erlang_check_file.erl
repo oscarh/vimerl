bf19416486fd5b887db7b53e2ed8183a533e42ba
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-02-22 09:37:46 +0100

    Foolish fix

--- compiler/erlang_check_file.erl
+++ compiler/erlang_check_file.erl
@@ -2,4 +2,4 @@
 
 main([Filename]) ->
 	compile:file(Filename, [basic_validation, report, {i, "include"},
-			{i, "../include"}, {d, "TEST"}, {d, "DEBUG"}]).
+			{i, "../include"}, {d, 'TEST'}, {d, 'DEBUG'}]).
